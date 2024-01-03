cbd2dad03fe36b0ffe8ccbbb06dfa364dfa842b3
twinogre <glad2you@naver.com>
2024-01-03 16:07:29 +0900

    요구사항_1번완료

---
+++ src/main/java/com/exam/test/HomeController.java
@@ -0,0 +1,13 @@
+package com.exam.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/article/list";
+    }
+}
---
+++ src/main/java/com/exam/test/article/ArticleController.java
@@ -0,0 +1,14 @@
+package com.exam.test.article;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/article")
+public class ArticleController {
+    @GetMapping("/list")
+    public String list(){
+        return "article_list";
+    }
+}
